d49cf0624627ec7e8bd15daf1987405f7be295c1
ilyaermi <105663653+ilyaermi@users.noreply.github.com>
2023-01-18 09:02:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,5 @@ admin_list = [1021524873, 828458879]
 BOT_TOKEN = '5953176557:AAELkYdMdTWoR_2LZ74sisqCtaOFyVtLM64'  # @Test_Products111_bot
 db_path = f'{homeDir}\\db.db'
 
-products = {
-    'молоко': [
-        'дом',
-        'дом2']
-    ,
-    'сыр': [
-        'дом3',
-        'дом4'
-    ],
-
-}
+products = {'молоко': ['дом', 'дом2'],
+            'сыр': ['дом3', 'дом4'], }
